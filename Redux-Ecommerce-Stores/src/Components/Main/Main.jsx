af53cc40aba4bc911eea0cefd509e6fa4a11d0f3
deepak.anand <deepakdanand@gmail.com>
2024-01-05 15:56:56 +0530

    Redux Store

---
+++ Redux-Ecommerce-Stores/src/Components/Main/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import NavigateButtons from '../NavigateButtons/NavigateButtons'
+import Slider from './../Slider/Slider';
+import ProductSection from '../ProductSection/ProductSection';
+import Footer from '../Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar />
+            <Slider />
+            <NavigateButtons />
+            <ProductSection />
+            <Footer />
+        </div>
+    )
+}
+
+export default Main
