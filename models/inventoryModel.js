b955242663a4d2ea53c7111483ec37e3c49674c0
deepak.anand <deepakdanand@gmail.com>
2024-01-05 10:42:26 +0530

    Index Commit

---
+++ models/inventoryModel.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const inventorySchema = new mongoose.Schema(
+  {
+    inventoryType: {
+      type: String,
+      required: [true, "inventory type require"],
+      enum: ["in", "out"],
+    },
+    bloodGroup: {
+      type: String,
+      required: [true, "blood group is required"],
+      enum: ["O+", "O", "AB+", "AB-", "A+", "A-", "B+", "B-"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "blood quantity is require"],
+    },
+    organisation: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: [true, "organisation is require"],
+    },
+    hospital: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: function () {
+        return this.inventroyType === "out";
+      },
+    },
+    donar: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: function () {
+        return this.inventroyType === "in";
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const inventoryModel = mongoose.model("Inventory", inventorySchema);
+export default inventoryModel;
